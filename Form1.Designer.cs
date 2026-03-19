2eb7166c8e012bee7c6da788171064f5144cba88
Choi Kyungseo <kyungseo0612@suwon.ac.kr>
2026-03-19 12:36:23 +0900

    과제1 코딩과 테스트 완료 UI구성 -Label, TextBox, Button, ListBox를 배치함 기능 명명 -Label, TextBox, Button, ListBox에 적절한 이름을 정함 전송 기능 -전송 버튼 클릭 시 TextBox의 텍스트를 ListBox의 항목으로 추가 입력창 정리 -추가 직후 TextBox의 내용을 비워 다음 입력을 준비

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lblTitle = new Label();
+            lstChat = new ListBox();
+            txtInput = new TextBox();
+            btnSend = new Button();
+            SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("한컴 말랑말랑 Bold", 25.8F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            lblTitle.Location = new Point(30, 26);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(344, 56);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "EchoMessenger";
+            lblTitle.Click += lblTitle_Click;
+            // 
+            // lstChat
+            // 
+            lstChat.Font = new Font("한컴 고딕", 19.7999973F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            lstChat.FormattingEnabled = true;
+            lstChat.Location = new Point(30, 102);
+            lstChat.Name = "lstChat";
+            lstChat.Size = new Size(739, 262);
+            lstChat.TabIndex = 1;
+            // 
+            // txtInput
+            // 
+            txtInput.Font = new Font("한컴 말랑말랑 Bold", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            txtInput.Location = new Point(30, 388);
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(586, 50);
+            txtInput.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            btnSend.BackColor = Color.Yellow;
+            btnSend.Font = new Font("한컴 말랑말랑 Bold", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnSend.Location = new Point(622, 388);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(147, 50);
+            btnSend.TabIndex = 3;
+            btnSend.Text = "전송";
+            btnSend.UseVisualStyleBackColor = false;
+            btnSend.Click += btnSend_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.AppWorkspace;
+            ClientSize = new Size(800, 472);
+            Controls.Add(btnSend);
+            Controls.Add(txtInput);
+            Controls.Add(lstChat);
+            Controls.Add(lblTitle);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblTitle;
+        private ListBox lstChat;
+        private TextBox txtInput;
+        private Button btnSend;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,24 @@ namespace EchoMessenger
         {
             InitializeComponent();
         }
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            //TextBox에 입력된 텍스트를 변수에 저장
+            string typed_msg = txtInput.Text;
+
+            //입력받은 문자열을 ListBox의 Items에 추가
+            lstChat.Items.Add(typed_msg);
+
+            //추가 직후 다음 입력을 준비하기 위해 TextBox의 내용을 비움
+            txtInput.Clear();
+        }
+
+       
     }
 }
